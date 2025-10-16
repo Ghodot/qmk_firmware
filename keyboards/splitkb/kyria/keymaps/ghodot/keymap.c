edd5ad94a0af8ddf7466d66f6f21a564335660cd
Nicolas Dol <ghodot@nicolasdol.net>
2025-10-17 01:54:17 +0200

    Add numbers layer

--- keyboards/splitkb/kyria/keymaps/ghodot/keymap.c
+++ keyboards/splitkb/kyria/keymaps/ghodot/keymap.c
@@ -19,6 +19,7 @@ enum layers {
     _QWERTY = 0,
     _NAV_DEFAULT,
     _NAV,
+    _NUMBERS,
     _SYM,
     _FUNCTION,
     _ADJUST,
@@ -30,6 +31,7 @@ enum layers {
 
 #define SYM      MO(_SYM)
 #define NAV      MO(_NAV)
+#define NUMBERS  MO(_NUMBERS)
 #define FKEYS    MO(_FUNCTION)
 #define ADJUST   MO(_ADJUST)
 
@@ -61,14 +63,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(
      KC_TAB  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y,   KC_U ,  KC_I ,   KC_O ,  KC_P , KC_BSPC,
      KC_ESC , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H,   KC_J ,  KC_K ,   KC_L , KC_SCLN, KC_ENT,
-     KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC,KC_CAPS,     FKEYS  , KC_RBRC, KC_N,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH, KC_RSFT,
-                                ADJUST , KC_LGUI, ALT_ENT, LCTL_T(KC_SPC) , KC_LGUI ,     LM(_NAV, MOD_RALT) , KC_SPC ,KC_RALT, KC_RGUI, KC_APP
+     KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC,KC_CAPS,           _______  , KC_RBRC, KC_N,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH, KC_RSFT,
+                                ADJUST , NUMBERS, ALT_ENT, LCTL_T(KC_SPC) , KC_LGUI ,     LM(_NAV, MOD_RALT) , KC_SPC ,KC_RALT, KC_RGUI, KC_APP
     ),
 
 [_NAV] = LAYOUT(
        _______, _______, _______, _______, _______, _______,                                     _______, _______, _______, _______, _______, KC_DEL,
        _______, _______, _______, _______, _______, _______,                                     KC_LEFT, KC_DOWN, KC_UP  ,   KC_RIGHT, _______, _______,
        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+                                  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+     ),
+
+
+
+[_NUMBERS] = LAYOUT(
+       _______, _______, _______, _______, _______, _______,                                     _______, KC_7,   KC_8,   KC_9,   KC_0,   _______,
+       _______, _______, _______, _______, _______, _______,                                     _______, KC_4,   KC_5,   KC_6,   _______, _______,
+       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_1,   KC_2,   KC_3,   KC_DOT, _______,
                                   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
      ),
 /*
